8866e8a7eb5a24ff9532f760bdc55adc9e2ecbed
ritamganguli <35348707+ritamganguli@users.noreply.github.com>
2024-02-19 15:21:11 +0530

    push

---
+++ src/test/java/com/lambdatest/TestNGTodo2.java
@@ -0,0 +1,75 @@
+package com.lambdatest;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNGTodo2 {
+
+    private RemoteWebDriver driver;
+    private String Status = "failed";
+
+    @BeforeMethod
+    public void setup(Method m, ITestContext ctx) throws MalformedURLException {
+        String username ="shubhamr";
+        String authkey ="dl8Y8as59i1YyGZZUeLF897aCFvIDmaKkUU1e6RgBmlgMLIIhh";
+        ;
+        
+        /*
+        Steps to run Smart UI project (https://beta-smartui.lambdatest.com/)
+        Step - 1 : Change the hub URL to @beta-smartui-hub.lambdatest.com/wd/hub
+        Step - 2 : Add "smartUI.project": "<Project Name>" as a capability above
+        Step - 3 : Add "((JavascriptExecutor) driver).executeScript("smartui.takeScreenshot");" code wherever you need to take a screenshot
+        Note: for additional capabilities navigate to https://www.lambdatest.com/support/docs/test-settings-options/
+        */
+
+        String hub = "@hub.lambdatest.com/wd/hub";
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platform", "Windows 10");
+        caps.setCapability("browserName", "chrome");
+        caps.setCapability("version", "latest");
+        caps.setCapability("build", "TestNG With Java");
+        caps.setCapability("name", m.getName() + this.getClass().getName());
+        caps.setCapability("plugin", "git-testng");
+        caps.setCapability("tunnel",true);
+
+        /*
+        Enable Smart UI Project
+        caps.setCapability("smartUI.project", "<Project Name>");
+        */
+
+        String[] Tags = new String[] { "Feature", "Magicleap", "Severe" };
+        caps.setCapability("tags", Tags);
+
+        driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), caps);
+    }
+
+    @Test
+    public void basicTest() throws InterruptedException {
+        String spanText;
+        System.out.println("Loading Url");
+
+        driver.get("https://demo.playwright.dev/api-mocking/");
+        Thread.sleep(15000);
+
+        System.out.println("TestFinished");
+        String status="passed";
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.executeScript("lambda-status=" + Status);
+        driver.quit();
+    }
+
+}
